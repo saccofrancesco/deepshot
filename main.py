2dd0a13e7503e10c63ccc029d33bcbcdd454baee
francesco <francescosacco.github@gmail.com>
2025-04-02 23:40:42 +0200

    Added W bars % and fixed some typing / comments issues

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sklearn.ensemble
 import numpy as np
 from nicegui import ui
 from functools import lru_cache
+from itertools import product
+from colorsys import rgb_to_hsv
 
 # Loading the model
 model: sklearn.ensemble._forest.RandomForestClassifier = joblib.load(
@@ -72,8 +74,77 @@ stat_to_full_name_desc: dict[str, str] = {
     "poss": "Possessions (POSS)",
     "usg_pct": "Usage % (USG%)",
     "tov_to_poss": "Turnover-to-Possesion Ratio (TOV/POSS)",
-    "ft_to_poss": "Free Throw per Possesion (FT/POSS),"
+    "ft_to_poss": "Free Throw per Possesion (FT/POSS),",
 }
+
+# Teams color codes list
+team_color_codes: dict[str, list[str]] = {
+    "Atlanta Hawks": ["#e03a3e", "#c1d32f"],
+    "Boston Celtics": ["#007a33", "#ba9653", "#963821"],
+    "Brooklyn Nets": ["#000000"],
+    "Charlotte Hornets": ["#1d1160", "#00788c"],
+    "Chicago Bulls": ["#ce1141"],
+    "Cleveland Cavaliers": ["#860038", "#fdbb30"],
+    "Dallas Mavericks": ["#00538c", "#002b5e"],
+    "Denver Nuggets": ["#0e2240", "#fec524", "#8b2131", "#1d428a"],
+    "Detroit Pistons": ["#c8102e", "#1d42ba", "#002d62"],
+    "Golden State Warriors": ["#1d428a", "#ffc72c"],
+    "Houston Rockets": ["#ce1141"],
+    "Indiana Pacers": ["#002d62", "#fdbb30"],
+    "Los Angeles Clippers": ["#c8102e", "#1d428a"],
+    "Los Angeles Lakers": ["#552583", "#f9a01b"],
+    "Memphis Grizzlies": ["#5d76a9", "#12173f", "#f5b112"],
+    "Miami Heat": ["#98002e", "#f9a01b"],
+    "Milwaukee Bucks": ["#00471b", "#0077c0"],
+    "Minnesota Timberwolves": ["#0c2340", "#236192", "#78be20"],
+    "New Orleans Pelicans": ["#0c2340", "#c8102e", "#85714d"],
+    "New York Knicks": ["#006bb6", "#f58426"],
+    "Oklahoma City Thunder": ["#007ac1", "#ef3b24", "#002d62"],
+    "Orlando Magic": ["#0077c0"],
+    "Philadelphia 76ers": ["#006bb6", "#ed174c", "#002b5c"],
+    "Phoenix Suns": ["#1d1160", "#e56020", "#ffcd00", "#b95915"],
+    "Portland Trail Blazers": ["#e03a3e"],
+    "Sacramento Kings": ["#5a2d81", "#63727a"],
+    "San Antonio Spurs": ["#c4ced4", "#000000"],
+    "Toronto Raptors": ["#ce1141", "#b4975a"],
+    "Utah Jazz": ["#f9a01b"],
+    "Washington Wizards": ["#002b5c", "#e31837"],
+}
+
+
+# Function to get the best colors from a set of two list mapped by two team names
+def get_best_color_pair(team1: str, team2: str) -> tuple[str, str]:
+
+    # Convert the hex color code in to rgb
+    def hex_to_hsv(hex_color: str) -> tuple:
+        hex_color: str = hex_color.lstrip("#")
+        r, g, b = tuple(int(hex_color[i : i + 2], 16) / 255.0 for i in (0, 2, 4))
+        return rgb_to_hsv(r, g, b)
+
+    # Get the mapped colors
+    colors_team1: str = team_color_codes.get(team1, [])
+    colors_team2: str = team_color_codes.get(team2, [])
+
+    if not colors_team1 or not colors_team2:
+        raise ValueError("Invalid team names provided.")
+
+    best_pair = None
+    max_contrast: int = -1
+
+    # Extract the contrast for each combination
+    for color1, color2 in product(colors_team1, colors_team2):
+        _, s1, v1 = hex_to_hsv(color1)
+        _, s2, v2 = hex_to_hsv(color2)
+        contrast: float = abs(v1 - v2) + abs(s1 - s2)
+
+        # If the current contrast is > than the one before, those will be the new colors
+        # and the contrast will be the threshold
+        if contrast > max_contrast:
+            max_contrast: float = contrast
+            best_pair: tuple[str, str] = (color1, color2)
+
+    return best_pair
+
 
 # Storing stats that if lower are better:
 lower_better_stats: set = {"tov", "pf", "drtg", "tov_pct", "tov_to_poss"}
@@ -99,7 +170,6 @@ def extract_games(date: str) -> list[dict[str, str | int | float]]:
     return games
 
 
-# Search the last available stat for each team and append it as home and away
 @lru_cache(maxsize=128)
 # Search the last available stat for each team and append it as home and away
 def find_most_recent_stats(
@@ -108,22 +178,27 @@ def find_most_recent_stats(
     most_recent_row = None
     most_recent_date = None
 
+    # Open the specified file
     with open(file_path, mode="r", newline="") as file:
         reader: csv.reader = csv.reader(file)
 
         # Read the headers
         headers: list[str] = next(reader)
 
+        # Loop the rows to extract the date and stats
         for row in reader:
             date_str, team = row[0], row[1]
 
+            # If the name founded is equal to the inputed one
             if team == team_name:
                 current_date: str = datetime.datetime.strptime(date_str, "%Y-%m-%d")
 
+                # Check if the date of that much is the most recent
                 if most_recent_date is None or current_date > most_recent_date:
                     most_recent_date: str = current_date
                     most_recent_row: str = row
 
+    # If so return that game stats
     if most_recent_row:
         return headers[2:], most_recent_row[2:]
     else:
@@ -136,32 +211,46 @@ class GameCard(ui.card):
 
         # Initializing the super class
         super().__init__()
-        self.classes("m-4 p-8 rounded-2xl shadow-md border w-[650px]")
+        self.classes("m-4 p-10 rounded-2xl shadow-md border w-[650px]")
 
         # Arranging the info
         with self:
 
+            # Calculating the color for each team
+            home_color, away_color = get_best_color_pair(
+                game["home_team"], game["away_team"]
+            )
+
             # Row for Team Logos and "VS"
-            with ui.row().classes("items-center justify-between w-full mb-2"):
+            with ui.row().classes("items-center justify-between w-full"):
                 ui.image(f"./img/badges/{game['home_team']}.png").classes("w-32")
                 ui.label("VS").classes("text-lg font-semibold text-center w-20")
                 ui.image(f"./img/badges/{game['away_team']}.png").classes("w-32")
 
             # Row for Team Names and Win Probabilities
-            with ui.row().classes("items-center justify-between w-full mb-2"):
-                with ui.column().classes("items-start"):
-                    ui.label(game["home_team"]).classes("text-left text-md font-bold")
-                    ui.label(f"Win Prob: {game['home_prob']} %").classes(
-                        f"text-left text-md font-bold {'text-green-600' if game['home_prob'] > 50 else 'text-red-600'}"
-                    )
+            with ui.row(align_items="stretch").classes("justify-between w-full"):
+                with ui.column(align_items="start"):
+                    ui.label(game["home_team"]).classes("text-left text-lg font-bold")
+                    ui.label(f"W {game['home_prob']} %").classes(
+                        f"text-left text-lg font-bold"
+                    ).style(f"color: {home_color}")
 
-                with ui.column().classes("items-end"):
-                    ui.label(game["away_team"]).classes("text-right text-md font-bold")
-                    ui.label(f"Win Prob: {game['away_prob']} %").classes(
-                        f"text-right text-md font-bold {'text-green-600' if game['away_prob'] > 50 else 'text-red-600'}"
-                    )
+                with ui.column(align_items="end"):
+                    ui.label(game["away_team"]).classes("text-right text-lg font-bold")
+                    ui.label(f"W {game['away_prob']} %").classes(
+                        f"text-right text-lg font-bold"
+                    ).style(f"color: {away_color}")
 
-            # Wide & Rounded "See More" Expansion
+            # HTML element to create W % bars
+            with ui.element("div").classes("flex w-full h-6"):
+                ui.element("div").style(
+                    f"flex: {game['home_prob']}; background-color: {home_color}"
+                ).classes("rounded-md mr-1")
+                ui.element("div").style(
+                    f"flex: {game['away_prob']}; background-color: {away_color}"
+                ).classes("rounded-lg ml-1")
+
+            # Wide & Rounded "See More" Expansion toggle
             with ui.expansion().classes(
                 "w-full shadow-md bg-gray-100 rounded-2xl overflow-hidden mx-auto"
             ).props("duration=550 hide-expand-icon") as expansion:
@@ -329,7 +418,7 @@ with ui.element("div").classes("w-full h-full flex"):
     ):
         date_container: ui.element = ui.element("div")
 
-    with ui.element("div").classes("w-2/3 ml-auto h-full overflow-auto bg-red p-16"):
+    with ui.element("div").classes("w-2/3 ml-auto h-full overflow-auto p-16 bg-red"):
         cards_container: ui.element = ui.element("div")
 
     # Rendering the games list
@@ -344,8 +433,8 @@ with ui.element("div").classes("w-full h-full flex"):
             date: ui.date = (
                 ui.date(today)
                 .bind_value_to(games_list, "date")
-                .props("color=black")
                 .style("border-radius: 16px;")
+                .props("color=black")
             )
 
             predict_button: ui.button = (
