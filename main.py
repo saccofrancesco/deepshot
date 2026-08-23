6b3ab87fe2addd513c3ea47fe8d1c2931d8d2b06
Francesco Sacco <francescosacco.github@gmail.com>
2026-08-23 19:59:11 +0200

    fix(main): update date for today's NBA games

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ lower_better_stats: set = {"tov", "pf", "drtg", "tov_pct", "tov_to_poss"}
 
 # Get the next day NBA game
 year: datetime.datetime = datetime.datetime.now().strftime("%Y")
-today: datetime.datetime = f"{year}-04-12"
+today: datetime.datetime = f"{year}-10-20"
 
 
 # Function to retrieve and get the scheduled games for today
